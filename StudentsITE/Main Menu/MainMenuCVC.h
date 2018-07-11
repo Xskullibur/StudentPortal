bd6aada9f88a0dcb2441aa7e429ed0e740495c43
Student2 <student2@ccj712-18.local>
2018-07-11 12:57:35 +0800

    Main Menu In-Progess

---
+++ StudentsITE/Main Menu/MainMenuCVC.h
@@ -0,0 +1,13 @@
+//
+//  MainMenuCVC.h
+//  StudentsITE
+//
+//  Created by Student2 on 11/7/18.
+//  Copyright © 2018 Student2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainMenuCVC : UICollectionViewController
+
+@end
