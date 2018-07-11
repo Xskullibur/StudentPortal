4825c4edae47ae749505c1b7ce406cbf40a5c89d
Student2 <student2@ccj712-122.local>
2018-07-11 11:39:55 +0800

    Debut

---
+++ StudentsITE/StudentsITE/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StudentsITE
+//
+//  Created by Student2 on 11/7/18.
+//  Copyright © 2018 Student2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StudentsITE/StudentsITE/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  StudentsITE
+//
+//  Created by Student2 on 11/7/18.
+//  Copyright © 2018 Student2. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
