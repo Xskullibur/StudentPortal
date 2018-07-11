3c2e80c0f2b67dc927ca30f4216a3e22817029f0
Student2 <student2@ccj712-122.local>
2018-07-11 12:53:13 +0800

    Login

---
+++ StudentsITE/Login/LoginVC.h
@@ -0,0 +1,13 @@
+//
+//  LoginVC.h
+//  StudentsITE
+//
+//  Created by Student2 on 11/7/18.
+//  Copyright © 2018 Student2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginVC : UIViewController
+@property (strong,nonatomic) NSMutableArray *Users;
+@end
---
+++ StudentsITE/Login/LoginVC.m
@@ -0,0 +1,38 @@
+//
+//  LoginVC.m
+//  StudentsITE
+//
+//  Created by Student2 on 11/7/18.
+//  Copyright © 2018 Student2. All rights reserved.
+//
+
+#import "LoginVC.h"
+
+@interface LoginVC ()
+
+@end
+
+@implementation LoginVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [_Users addObject:<#(nonnull id)#>]
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
