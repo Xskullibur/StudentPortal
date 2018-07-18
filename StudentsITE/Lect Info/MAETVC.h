e1615278cbad5c94c6287df18265aa215d001c64
Student2 <student2@ccj712-122.local>
2018-07-18 11:34:49 +0800

    Lecturer Info Added (Not Navigated)

---
+++ StudentsITE/Lect Info/MAETVC.h
@@ -0,0 +1,15 @@
+//
+//  MAETVC.h
+//  StudentsITE
+//
+//  Created by Student2 on 17/7/18.
+//  Copyright © 2018 Student2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MAETVC : UITableViewController
+@property (weak, nonatomic) IBOutlet UIButton *btnNumber;
+- (IBAction)btnCall:(id)sender;
+
+@end
