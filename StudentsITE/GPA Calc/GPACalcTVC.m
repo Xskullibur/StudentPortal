cb1133bae119e2f4affdff8e8a2b79377a454485
Student2 <student2@ccj712-122.local>
2018-08-01 14:48:43 +0800

    Calculate Fix

--- StudentsITE/GPA Calc/GPACalcTVC.m
+++ StudentsITE/GPA Calc/GPACalcTVC.m
@@ -59,13 +59,16 @@
     }
 }
 
+-(void)viewDidDisappear:(BOOL)animated{
+    self.navigationItem.rightBarButtonItem.enabled=false;
+    self.navigationItem.rightBarButtonItem.enabled=true;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    
-    
     // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
+    //self.clearsSelectionOnViewWillAppear = YES;
     
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
     // self.navigationItem.rightBarButtonItem = self.editButtonItem;
